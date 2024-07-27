213e7a341f2fefb48d31fcf1d0e33544b9336b55
Belinda Chin <chinbelinda1@gmail.com>
2024-07-27 23:19:49 +0800

    Added display search result functionality

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,6 +1,49 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MdClear, MdSearch } from "react-icons/md";
+
+
+interface Suggestion {
+  stemmedQueryTerm: string;
+  suggestions: string[];
+}
+
+interface Highlight {
+  BeginOffset: number;
+  EndOffset: number;
+}
+
+interface DocumentText {
+  Highlights: Highlight[];
+  Text: string;
+}
+
+interface Result {
+  Id: string | null;
+  DocumentId: string;
+  DocumentExcerpt: DocumentText;
+  DocumentTitle: DocumentText;
+  DocumentURI: string;
+}
+
+interface QueryResult {
+  Page: number;
+  PageSize: number;
+  ResultItems: Result[];
+  TotalNumberOfResults: number
+}
+
+function GetSuggestions(queryString: string): Promise<Suggestion> {
+  const url = "https://gist.githubusercontent.com/yuhong90/b5544baebde4bfe9fe2d12e8e5502cbf/raw/e026dab444155edf2f52122aefbb80347c68de86/suggestion.json";
+  return fetch(url)
+    .then(response => response.json());
+}
+
+function GetSearchResult(searchTerm: string): Promise<QueryResult> {
+  const url = "https://gist.githubusercontent.com/yuhong90/b5544baebde4bfe9fe2d12e8e5502cbf/raw/44deafab00fc808ed7fa0e59a8bc959d255b9785/queryResult.json";
+  return fetch(url)
+    .then(response => response.json());
+}
 
 
 function Dropdown({show } : { show: boolean }) {
@@ -29,7 +72,6 @@ function SearchBar({ handleSearch }: { handleSearch: any }) {
   const [showDropdown, setShowDropdown] = useState(false);
 
   function handleSuggestions(searchTerm: string) {
-    console.log(`suggestions for ${searchTerm} should appear here!`);
     setShowDropdown(true);
   }
 
@@ -73,13 +115,60 @@ function SearchBar({ handleSearch }: { handleSearch: any }) {
 }
 
 function SearchResults({ searchTerm } : { searchTerm: string }) {
-  let description: string | undefined = undefined;
-  if (searchTerm !== "") {
-    description = `Searching for: ${searchTerm}`;
+  const [queryResult, setQueryResults] = useState<QueryResult | undefined>(undefined);
+
+  function Pagination({queryResult} : {queryResult: QueryResult | undefined}) {
+    if (queryResult) {
+      const start = queryResult.Page * queryResult.PageSize;
+      const end = start + queryResult.PageSize;
+      const total = queryResult.TotalNumberOfResults
+      return (
+        <div className="pagination">
+          <span>Showing {start}-{end} of {total} results</span>
+        </div>
+      )
+    }
+    return null;
   }
+
+  function SearchResult({result} : {result: Result}) {
+    return (
+      <div className="search-result">
+        <div className="search-result-title">
+          <span>{result.DocumentTitle.Text}</span>
+        </div>
+        <div className="search-result-excerpt">
+          <span>{result.DocumentExcerpt.Text}</span>
+        </div>
+        <div className="search-result-uri">
+          <a href={result.DocumentURI}>{result.DocumentURI}</a>
+        </div>
+      </div>
+    )
+  }
+
+  useEffect(() => {
+    if (searchTerm !== "") {
+      // setDescription(`Searching for: ${searchTerm}`);
+      GetSearchResult(searchTerm)
+      .then(resp => {
+        setQueryResults(resp);
+        // setDescription(queryResult.TotalNumberOfResults.toString());
+        // return queryResult.ResultItems
+      })
+      // .then(resultItems => {
+      //   setResults(resultItems);
+      // });
+      return () => {};
+    }
+  }, [searchTerm]);
+
   return (
     <div className="search-results">
-      {description}
+      <Pagination queryResult={queryResult} />
+      {queryResult?.ResultItems.map((result) => {
+        return <SearchResult key={result.DocumentId} result={result} />
+      })}
     </div>
   )
 }
