956f7b2ae73884cc7219b47697885fcbde5d3b74
Belinda Chin <chinbelinda1@gmail.com>
2024-07-27 15:24:25 +0800

    feature: add search and clear buttons to search bar

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,26 +1,69 @@
 
+import { useState } from "react";
+import { MdClear, MdSearch } from "react-icons/md";
 
-function SearchBar() {
+
+function SearchBar({ handleSearch }: { handleSearch: any }) {
+
+  function handleSuggestions(searchTerm: string) {
+    console.log(`suggestions for ${searchTerm} should appear here!`);
+  }
+
+  function handleClick(action: string) {
+    const inputEl = document.querySelector('input');
+    if (action === 'clear') {
+      if (inputEl) {
+        inputEl.value = "";
+      }
+      handleSearch("");
+    }
+    else if (action === "search") {
+      if (inputEl) {
+        handleSearch(inputEl.value);
+      }
+    }
+  }
+
   return (
-    <div className="search-bar">
-      <p>search bar goes here</p>
+    <div className="search-bar-background">
+      <div className="search-bar">
+        <input type="text" onChange={(e) => handleSuggestions(e.target.value)}/>
+        <button className="clear" onClick={() => handleClick('clear')}>
+          <MdClear />
+        </button>
+        <button className="search" onClick={() => handleClick('search')}>
+          <span className="label">
+            <MdSearch/> <span>Search</span>
+          </span>
+        </button>
+      </div>
     </div>
   )
 }
 
-function SearchResults() {
+function SearchResults({ searchTerm } : { searchTerm: string }) {
+  let description: string | undefined = undefined;
+  if (searchTerm !== "") {
+    description = `Searching for: ${searchTerm}`;
+  }
   return (
     <div className="search-results">
-      <p>search results go here</p>
+      {description}
     </div>
   )
 }
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  function handleSearch(searchTerm: string): void {
+    setSearchTerm(searchTerm);
+  }
+
   return (
     <div className="search">
-      <SearchBar />
-      <SearchResults />
+      <SearchBar handleSearch={handleSearch}/>
+      <SearchResults searchTerm={searchTerm} />
     </div>
   )
 }
