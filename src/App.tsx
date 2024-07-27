b0e85eae35ddada890f2743fc3bfc10c4b606d43
Belinda Chin <chinbelinda1@gmail.com>
2024-07-27 12:39:02 +0800

    Add masthead

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './App.css';
+import Masthead from './components/masthead/masthead';
 
 function App() {
   return (
     <div className="App">
+      <Masthead />
       <h1>search-portal</h1>
     </div>
   );
