4aac047f02371181f2fbb65af64cdb739a8d1ea7
Belinda Chin <chinbelinda1@gmail.com>
2024-08-01 18:06:56 +0800

    removed unnecessary acts from tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { act } from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import App from './App';
@@ -24,51 +24,53 @@ test('loads and renders search button', () => {
 });
 
 test('when the search bar input has more than or equal 2 non-whitespace characters, a dropdown appears with search suggestions', async () => {
+  const user = userEvent.setup();
   render(<App />);
-  await act(async () => {
-    const input = screen.getByRole("textbox", {name: "Search"});
-    fireEvent.change(input, {target: {value: "ch"}});
-  });
+  const input = screen.getByRole("textbox", {name: "Search"});
+  user.type(input, "ch");
   const suggestions = await screen.findByRole("generic", {name: "Suggestions dropdown"});
   expect(suggestions).toBeVisible();
 });
 
 test('when the search bar input has less than 2 characters, a dropdown appears with search suggestions', async () => {
+  const user = userEvent.setup();
   render(<App />);
-  await act(async () => {
-    const input = screen.getByRole("textbox", {name: "Search"});
-    fireEvent.change(input, {target: {value: "c"}});
-  });
-  const suggestions = await screen.queryByRole("generic", {name: "Suggestions dropdown"});
+  const input = screen.getByRole("textbox", {name: "Search"});
+  user.type(input, "c");
+  const suggestions = screen.queryByRole("generic", {name: "Suggestions dropdown"});
   expect(suggestions).toBeNull();
 });
 
 test('when the search bar is empty, clicking on search returns no search results', async () => {
+  const user = userEvent.setup();
   render(<App />);
-  await act(async () => {
-    userEvent.click(screen.getByRole('button', {name: 'Search'}));
-  });
-  const searchResults = await screen.queryByText(/showing/i);
+  const button = screen.getByRole('button', {name: 'Search'});
+  user.click(button);
+  const searchResults = screen.queryByText(/showing/i);
   expect(searchResults).toBeNull();
 });
 
 test('when the search bar input has at least one non-whitespace character, clicking on search returns search results', async () => {
+  const user = userEvent.setup();
   render(<App />);
+  const input = screen.getByRole("textbox", {name: "Search"});
+  const button = screen.getByRole("button", {name: "Search"});
   await act(async () => {
-    const input = screen.getByRole("textbox", {name: "Search"});
-    fireEvent.change(input, {target: {value: "c"}});
-    userEvent.click(screen.getByRole('button', {name: 'Search'}));
+    await user.type(input, "c");
+    await user.click(button);
   });
   const searchResults = await screen.findByText(/showing/i);
   expect(searchResults).toBeInTheDocument();
 });
 
 test('when the search bar input only has whitespace characters, clicking on search returns no search results', async () => {
+  const user = userEvent.setup();
   render(<App />);
+  const input = screen.getByRole("textbox", {name: "Search"});
+  const button = screen.getByRole("button", {name: "Search"});
   await act(async () => {
-    const input = screen.getByRole("textbox", {name: "Search"});
-    fireEvent.change(input, {target: {value: "    "}});
-    userEvent.click(screen.getByRole('button', {name: 'Search'}));
+    await user.type(input, "    ");
+    await user.click(button);
   });
   const noResultsFound = await screen.findByText(/No results found/i);
   expect(noResultsFound).toBeDefined();
@@ -76,31 +78,32 @@ test('when the search bar input only has whitespace characters, clicking on sear
 
 test('when the search bar input is empty, the clear button does not appear', async () => {
   render(<App />);
-  await act(async () => {
-    const input = screen.getByRole("textbox", {name: "Search"});
-    fireEvent.change(input, {target: {value: ""}});
-  });
-  const clearButton = await screen.queryByRole("button", {name: "Clear button"});
+  const input = screen.getByRole("textbox", {name: "Search"});
+  expect(input).toHaveValue("");
+  const clearButton = screen.queryByRole("button", {name: "Clear button"});
   expect(clearButton).toBeNull();
 });
 
 test('when the search bar input is not empty, the clear button appears', async () => {
-  render(<App />);
-  await act(async () => {
-    const input = screen.getByRole("textbox", {name: "Search"});
-    fireEvent.change(input, {target: {value: "test"}});
-  });
-  const clearButton = await screen.findByRole("button", {name: "Clear button"});
-  expect(clearButton).toBeInTheDocument();
-});
-
-test('clicking on the clear button clears the input in the search bar', async () => {
+  const user = userEvent.setup();
   render(<App />);
   const input = screen.getByRole("textbox", {name: "Search"});
   await act(async () => {
-    fireEvent.change(input, {target: {value: "test"}});
-    const clearButton = await screen.findByRole("button", {name: "Clear button"});
-    userEvent.click(clearButton);
+    await user.type(input, "helloworld");
   });
+  const clearButton = screen.queryByRole("button", {name: "Clear button"});
+  expect(clearButton).not.toBeNull();
+});
+
+test('clicking on the clear button clears the input in the search bar', async () => {
+  const user = userEvent.setup();
+  render(<App />);
+  const input = screen.getByRole("textbox", {name: "Search"});
+  await act(async () => {
+    await user.type(input, "test");
+  });
+  expect(input).toHaveValue("test");
+  const clearButton = await screen.findByRole("button", {name: "Clear button"});
+  userEvent.click(clearButton);
   expect(input.nodeValue).toBeNull();
 });
